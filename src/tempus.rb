b077ef2e2dce9e14d88b66150bca4062b7fca0cd
André <rs.andre@gmail.com>
2012-12-05 13:43:48 -0200

    adding private methods

--- src/tempus.rb
+++ src/tempus.rb
@@ -1,13 +1,31 @@
 class Tempus
  
+
   def parse(data)
-    @resources = this.getResources(data)
-    @processes = this.getProcesses(data) 
-    @rules = this.getRules(data)
+    @resources = this.parseResources(data)
+    @processes = this.parseProcesses(data) 
+    @rules = this.parseRules(data)
     [@resources, @processes, @rules].all?{|i| i.is_a?(Array) && i.length > 0}
   end
 
   def solve
   end
+  
+  private
+  
+  def parseResources(data)
+    throw :not_implemented_yet
+  end
+  def parseProcesses(data)
+    throw :not_implemented_yet
+  end
+  def parseRules(data)
+    throw :not_implemented_yet
+  end
+
+      
+     
+    
+
 
 end
