c9f8d88a3b9d1a599d6ae52403ee76100babc66f
André <rs.andre@gmail.com>
2012-12-12 00:42:39 -0200

    implementing solver

--- src/solver/solution.rb
+++ src/solver/solution.rb
@@ -1,5 +1,12 @@
 class Solution
-  def initialize
+  
+  def initialize(time_slots,resources)
     @time_slots_to_resources = {}
+    time_slots.each { |time_slot|
+      @time_slots_to_resources[time_slot] = []
+      resources.each { |resource|
+        @time_slots_to_resources[time_slot] << resource if rand(2) == 0
+      }
+    }
   end
 end
--- src/solver/solver.rb
+++ src/solver/solver.rb
@@ -1,9 +1,23 @@
 module Solver
-  def solve(time_slots,resources)
-    result = []
-    resources.permutation(time_slots.length) { |e| 
-      result << e if true #TODO: substitute "true" for rules
-    }
-    result
+
+  def solve(time_slots,resources, hard_rules, soft_rules)
+    solution_to_test = this.solution_candidate(time_slots,resources)
+    until hard_rules.all?{|hard_rule| hard_rules.applies? solution_to_test} do
+      $log.info("Trying to satisfy all hard rules")
+    end
+    raise "Solver#solve soft rules test not implemented yet. missing loop with yaml defined satistaction cut"
   end
+  
+  def solution_candidate(time_slots,resources)
+    Solution.new(time_slots,resources)
+  end
+
+  #def solve_original(time_slots,resources)
+  #  result = []
+  #  resources.permutation(time_slots.length) { |e| 
+  #    result << e if true #TODO: substitute "true" for rules
+  #  }
+  #  result
+  #end
+
 end
