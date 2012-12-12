5bf43dac60d6f1b09a853f951fea74d31920bcc7
leandro.bogoni <leandro.bogoni@procempa.com.br>
2012-12-12 17:40:20 -0200

    adding config file

--- src/tempus.rb
+++ src/tempus.rb
@@ -1,13 +1,24 @@
 require 'logger'
+require 'yaml'
 
-$log = Logger.new(STDOUT)
+$config = nil
 
-require_relative "./solver/solver.rb"
-require_relative "./solver/rule.rb"
-require_relative "./parser/parser.rb"
-
-
-class Tempus 
-include Solver, Parser
-
+parsed = begin
+  $config = YAML.load(File.open("src/config.yml"))
+rescue ArgumentError => e
+  puts "Could not parse YAML: #{e.message}"
 end
+
+p $config
+
+#$log = Logger.new(STDOUT)
+#
+#require_relative "./solver/solver.rb"
+#require_relative "./solver/rule.rb"
+#require_relative "./parser/parser.rb"
+#
+#
+#class Tempus 
+#include Solver, Parser
+#
+#end
