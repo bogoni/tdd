d404b79005acac1cb6e017637f5fadbc7bcf44fc
leandro.bogoni <leandro.bogoni@procempa.com.br>
2012-12-13 16:26:14 -0200

    moving to rails (so what?)

---
+++ src/tempusWebServer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TempusWebServer::Application.config.secret_token = 'd9ac631fe5548f6ea781330c4a192c2b570e6be8f5cc9a8adefa7f0676d1f52d8e8faca62f69ab7ab0fe5d84ecd0cb04cf2110ff54d80161bfbac166d80281ff'
