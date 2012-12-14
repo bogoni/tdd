9c6f8b385dc5bec7ce60e06f8f2a919980682a03
leandro.bogoni <leandro.bogoni@procempa.com.br>
2012-12-14 12:01:13 -0200

    adding timetable

---
+++ src/tempusWebServer/app/models/timetable.rb
@@ -0,0 +1,3 @@
+class Timetable < ActiveRecord::Base
+  attr_accessible :name
+end
--- src/tempusWebServer/config/routes.rb
+++ src/tempusWebServer/config/routes.rb
@@ -1,4 +1,7 @@
 TempusWebServer::Application.routes.draw do
+  resources :timetables
+
+
   get "home/index"
 
   # The priority is based upon order of creation:
