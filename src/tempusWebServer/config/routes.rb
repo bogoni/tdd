67af0512367ecfa998898c579ea9aa53137e5101
leandro.bogoni <leandro.bogoni@procempa.com.br>
2012-12-14 11:41:30 -0200

    initial routing

--- src/tempusWebServer/config/routes.rb
+++ src/tempusWebServer/config/routes.rb
@@ -1,4 +1,6 @@
 TempusWebServer::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -47,8 +49,7 @@ TempusWebServer::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
