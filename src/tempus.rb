3f8683954c8779731f9f6badadd09a947c809f1a
André <rs.andre@gmail.com>
2012-12-17 16:32:59 -0200

    removing solver

--- src/tempus.rb
+++ src/tempus.rb
@@ -11,12 +11,3 @@ end
 
 $log = Logger.new(STDOUT)
 
-require_relative "./solver/solver.rb"
-require_relative "./solver/rule.rb"
-require_relative "./parser/parser.rb"
-
-
-class Tempus 
-include Solver, Parser
-
-end
